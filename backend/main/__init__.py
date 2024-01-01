9bfc39b9b97dab658991ffaca2e3822afe85a69e
Ignacio Cruz <ignaciojcruz@hotmail.com>
2024-01-01 20:03:08 -0300

    Add configuracion para DB

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -5,18 +5,35 @@ from dotenv import load_dotenv
 #Importo el modulo para crear la api-rest
 from flask_restful import Api
 
+#Importo el modulo para conectarme con una base SQL
+from flask_sqlalchemy import SQLAlchemy
+
 api = Api()
+
+db = SQLAlchemy()
 
 def create_app():
 
     app = Flask(__name__)
 
+    #cargo las variables de entorno
     load_dotenv()
+
+    #Configuración de la base de datos
+    PATH = os.getenv("DATABASE_PATH")
+    DB_NAME = os.getenv("DATABASE_NAME")
+    if not os.path.exists(f'{PATH}{DB_NAME}'):
+        os.chdir(f'{PATH}')
+        file = os.open(f'{DB_NAME}', os.O_CREAT)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{PATH}{DB_NAME}'
+    db.init_app(app)
 
     import main.resources as resources
     api.add_resource(resources.ClientesResource, '/clientes')
     api.add_resource(resources.ClienteResource, '/cliente/<id>')
 
     api.init_app(app)
-
+ 
     return app
